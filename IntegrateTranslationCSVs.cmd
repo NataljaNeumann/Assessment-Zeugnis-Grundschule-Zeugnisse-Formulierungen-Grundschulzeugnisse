cf776d20dca795e9ff898473d0f8809f5c680008
NataljaNeumann <NataljaNeumann@gmx.de>
2025-11-28 00:31:55 +0100

    Improved localization scripts

--- IntegrateTranslationCSVs.cmd
+++ IntegrateTranslationCSVs.cmd
@@ -1,3 +1,3 @@
 ResxVsCsv --directory School-Report\Forms --toresx SchoolReportForm.csv 
-ResxVsCsv --directory School-Report\Forms --toresx TextChangeForm.csv v 
+ResxVsCsv --directory School-Report\Forms --toresx TextChangeForm.csv
 pause
